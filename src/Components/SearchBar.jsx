995309d72b69c57de1850b210ce8df217418b551
David Solano <dsolano@itnnetworks.com>
2023-01-04 15:04:44 -0500

    improved main search bar and mapped books

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,11 +1,30 @@
-
-const SearchBar = ({placeholder}) => {
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import "./SearchBar.css";
+import data from "../data.json";
+const SearchBar = ({ placeholder }) => {
   return (
-    <div className="search">
-      <input type="text" placeholder={placeholder}/>
-      <img src='../assets/favicon.svg' alt="Search" />
-    </div>
-  )
-}
+    <>
+      <div className="main-container-search">
+        <div className="search">
+          <SearchIcon className="icon" fontSize="large" />
+          <input type="text" placeholder={placeholder} />
+          <CloseIcon className="icon" fontSize="large" />
+        </div>
+        <div className="line"></div>
 
-export default SearchBar
+        <div className="result-bar">
+          {data.map((b) => {
+            return (
+              <a href={b.link} className="book-link" target={"_blank"} key={b.title+b.year} >
+                <h2 className="book-title">{b.title}</h2>
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
